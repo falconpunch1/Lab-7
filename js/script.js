acdc18d367e4f56e92c7eb77f754363b418f1100
falconpunch1 <dmedina10@hawk.iit.edu>
2025-11-11 13:31:32 -0600

    Update script.js
    Reverting changes made to the removal of function init()

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-/*  Initializing variables for event listener   */
-button = document.getElementById('entrybutton');
-textbox = document.getElementById('entryinput');
-results = document.getElementById('textoutput');
+function init(){
+    /*  Initializing variables for event listener   */
+    var button = document.getElementById('entrybutton');
+    var textbox = document.getElementById('entryinput');
+    var results = document.getElementById('textoutput');
 
-/*  Adding event listener, adding alert and results functionality   */
-button.addEventListener("click", function(){
-    alert("David Medina: " + textbox.value);
-    results.textContent = textbox.value;
-});
+    /*  Adding event listener, adding alert and results functionality   */
+    button.addEventListener("click", function(){
+        alert("David Medina: " + textbox.value);
+        results.textContent = textbox.value;
+    });
+
+}
+
+window.addEventListener('load', init)
